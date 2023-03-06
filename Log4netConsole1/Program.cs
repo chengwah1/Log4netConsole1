6475004d3c3cdb3231df5386bf3d12c5d9fa002f
Cheng Wah SING <Cheng_Wah_Sing_from.AVEPOINT@edb.sg>
2023-03-06 16:28:15 +0800

    Add project files.

---
+++ Log4netConsole1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+namespace Log4netConsole1
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+        private static readonly Guid _requestId = Guid.NewGuid();
+        static void Main(string[] args)
+        {
+            log4net.ThreadContext.Properties["RequestId"] = _requestId;
+
+            // Use log4net to log a message
+            _log.Info("Hello, world!");
+            _log.Info("Hello logging world!");
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
